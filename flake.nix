c6e150677a4876961c55f7ea7550acb4d0451b56
useEffects <joel.sr1024@gmail.com>
2023-12-11 00:39:31 +0530

    changes

--- flake.nix
+++ flake.nix
@@ -9,22 +9,15 @@
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      # TODO please change the hostname to your own
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./nixos/configuration.nix
-          # make home-manager as a module of nixos
-          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
-            # TODO replace ryan with your own username
             home-manager.users.april = import ./home-manager/home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
         ];
       };
--- home-manager/vscode/user-settings.nix
+++ home-manager/vscode/user-settings.nix
@@ -4,11 +4,11 @@
   "window.zoomLevel" = 2;
   "files.autoSave" = "afterDelay";
   "workbench.colorTheme" = "Catppuccin Mocha";
-  "java.jdt.ls.java.home" = "/nix/store/gfka0gfbh7yy2f8dzsk0i30lx9hmmzfa-openjdk-17.0.7+7/lib/openjdk";
+  "java.jdt.ls.java.home" = "${pkgs.openjdk17}/lib/openjdk";
   "java.configuration.runtime" = [
     {
       "name" = "JavaSE-17";
-      "path" = "/nix/store/gfka0gfbh7yy2f8dzsk0i30lx9hmmzfa-openjdk-17.0.7+7/lib/openjdk";
+      "path" = "${pkgs.openjdk17}/lib/openjdk";
       "default" = true;
     }
   ];
